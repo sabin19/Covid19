c0087ac1f274a3971d460b8f2979bb79366c1db5
Sabin <developer.sabinmon@gmail.com>
2020-07-10 10:51:45 +0530

    Hilt added to project

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
-    val versionName = "7.0.15" // X.Y.Z; X = Major, Y = minor, Z = Patch level
-    private val versionCodeBase = 70150 // XYYZZM; M = Module (tv, mobile)
-    val versionCodeMobile = versionCodeBase + 3
+    val versionName = "1.0.0" // X.Y.Z; X = Major, Y = minor, Z = Patch level
+    private val versionCodeBase = 1 // XYYZZM; M = Module (tv, mobile)
+    val versionCodeMobile = versionCodeBase + 1
 
     const val COMPILE_SDK = 29
     const val TARGET_SDK = 29
@@ -9,7 +9,6 @@ object Versions {
 
     const val ANDROID_GRADLE_PLUGIN = "4.0.0"
     const val BENCHMARK = "1.0.0"
-    const val FABRIC = "1.31.2"
     const val GOOGLE_SERVICES = "4.3.3"
     const val KOTLIN = "1.3.72"
     const val NAVIGATION = "2.2.2"
